deebf4ce3b3170085c74232fc1d5880b147cf44b
howyda ahmed <howydaahmedabueila@gmail.com>
2025-07-26 10:42:19 +0300

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://github.com/howydaahmed2023/papa-john-s-angular-project',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
